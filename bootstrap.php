05b03652bb78b82857673ee2c0360515b9f45436
Alexandre Tomasia <alexandre.tomasia@gmail.com>
2020-03-08 15:02:19 +0100

    feat: twig config

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+// Twig Config
+require_once './vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('./src/views/templates');
+$twig = new \Twig\Environment($loader, [
+    'cache' => './var/cache/twig',
+]);
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+// Temporary
+include ('bootstrap.php');
+
+echo $twig->render('index/index.html.twig', ['page' => 'accueil']);
