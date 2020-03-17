2ae453e20517bf8cc2b9badad326133bba73f403
Alexandre Tomasia <alexandre.tomasia@gmail.com>
2020-03-17 21:10:41 +0100

    feat: added basic routing and login page template

---
+++ src/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+class IndexController{
+
+    public static function indexAction(){
+        require 'bootstrap.php';
+
+        echo $twig->render('index/index.html.twig', ['page' => 'accueil']);
+    }
+
+    public static function pageNotFoundAction(){
+        require 'bootstrap.php';
+
+        echo $twig->render('layout/page-not-found.html.twig');
+    }
+}
