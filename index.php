3e60c31fe1a575ad958118a20b4b25a5fdeaa41a
Alexandre Tomasia <alexandre.tomasia@gmail.com>
2020-03-18 11:24:46 +0100

    feat: mods on the router

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
-
+// An htaccess file has been configured to redirect all request to this file
+// This file will be our router
 
 $request = $_SERVER['REQUEST_URI'];
 var_dump($request);
 
+// For test purpose
 if( preg_match('/^(\/popotier\/recette\/)[0-9]+/', $request)){
     $splitRequest = explode('/', $request);
     var_dump($splitRequest);die;
@@ -14,12 +16,15 @@ switch ($request) {
         require_once 'src/controllers/IndexController.php';
         IndexController::indexAction();
         break;
+
+    case '/popotier/home':
+        require_once 'src/controllers/IndexController.php';
+        IndexController::indexAction();
+        break;
+
     case '/popotier/login':
         require_once 'src/controllers/LoginController.php';
         LoginController::indexAction();
-        break;
-    case '/about':
-        require __DIR__ . '/views/about.php';
         break;
 
     default:
@@ -29,4 +34,3 @@ switch ($request) {
         break;
 }
 
-//echo $twig->render('index/index.html.twig', ['page' => 'accueil']);
