8752ca87df1d27f655ebe879c53b2009a04cf343
Alexandre Tomasia <alexandre.tomasia@gmail.com>
2020-03-18 11:42:12 +0100

    feat: added namespaces

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
+
 // An htaccess file has been configured to redirect all request to this file
 // This file will be our router
+include 'src/controllers/IndexController.php';
+include 'src/controllers/LoginController.php';
+
+use Src\Controllers\IndexController;
+use Src\Controllers\LoginController;
 
 $request = $_SERVER['REQUEST_URI'];
 var_dump($request);
@@ -13,23 +19,19 @@ if( preg_match('/^(\/popotier\/recette\/)[0-9]+/', $request)){
 
 switch ($request) {
     case '/popotier/' :
-        require_once 'src/controllers/IndexController.php';
-        IndexController::indexAction();
+        header('Location: /popotier/home');
         break;
 
     case '/popotier/home':
-        require_once 'src/controllers/IndexController.php';
         IndexController::indexAction();
         break;
 
     case '/popotier/login':
-        require_once 'src/controllers/LoginController.php';
         LoginController::indexAction();
         break;
 
     default:
         http_response_code(404);
-        require_once 'src/controllers/IndexController.php';
         IndexController::pageNotFoundAction();
         break;
 }
--- src/controllers/IndexController.php
+++ src/controllers/IndexController.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Src\Controllers;
+
 class IndexController{
 
     public static function indexAction(){
