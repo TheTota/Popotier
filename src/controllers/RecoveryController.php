729d9bc4529c2507faa750463a9c4a9e0346a12e
Thomas Cianfarani <thomas.ci83@gmail.com>
2020-05-11 15:33:38 +0200

    Add recovery page and format login/signup pages

---
+++ src/controllers/RecoveryController.php
@@ -0,0 +1,17 @@
+<?php
+namespace src\controllers;
+
+use src\services\LoginService;
+use src\utils\Templater;
+
+
+
+class RecoveryController{
+
+    public function view(){
+        $twig = Templater::getInstance()->getTwig();
+
+        echo $twig->render('login/password-recovery.html.twig', []);
+    }
+
+}
